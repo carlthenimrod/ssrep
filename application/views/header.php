fd70931f5da5e77bf0c640fc1d3f7d28e94249bb
Carl Dawson <carl@invisible-gorilla.com>
2016-10-04 21:04:49 -0400

    Import fixes

--- application/views/header.php
+++ application/views/header.php
@@ -2,15 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Rebar &amp; Wire Mesh Rolls &amp; Sheets - Naperville, Illinois - Rep Locator</title>
+	<title>Rep Locator</title>
 	
 	<link href="<?= base_url('assets/css/chosen.min.css'); ?>" rel="stylesheet" />
 	<link href="<?= base_url('assets/css/style.css'); ?>" rel="stylesheet" />
-
-	<link rel="stylesheet" href="/css/html5-style.css" />
-
-	<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans%3A300%2C300italic%2Cregular%2Citalic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%7CLato%3A100%2C100italic%2C300%2C300italic%2Cregular%2Citalic%2C700%2C700italic%2C900%2C900italic" media="screen">
-	<link href="http://fonts.googleapis.com/css?family=Open+Sans%3A300%2C300italic%2Cregular%2Citalic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%7CLato%3A100%2C100italic%2C300%2C300italic%2Cregular%2Citalic%2C700%2C700italic%2C900%2C900italic" type="text/css" rel="stylesheet">
 
 	<!--[if IE]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -24,72 +19,5 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="<?= base_url('assets/js/chosen.jquery.min.js'); ?>"></script>
 	<script src="<?= base_url('assets/js/map.js'); ?>"></script>
-	<script src="/js/browser-detect.js"></script>
 </head>
 <body>
-	<header>
-         <!--header inner wrap start-->
-         <div class="inner-wrap">
-            <!--header logo start--> <a href="/index.html" class="header-logo"><img src="/images/metal-partners-rebar-logo.jpg" title="Metal Partners Rebar" alt="Metal Partners Rebar"></a>
-            <!--header logo end-->
-            <!--header right start-->
-            <div class="header-right"><!--top nav1 start-->
-               <nav class="top-nav1 cms_menu_1">
-                  <a href="/index.html">Home</a> <a href="/about.html" rel="nofollow">About Us</a> <a href="/contact.html" rel="nofollow">Contact Us</a> <a href="/request-information.html" rel="nofollow">Request Information</a> <a class="last" href="https://metalpartnersrebar.wordpress.com/" rel="nofollow" target="_blank">Blog</a>
-               </nav>
-               <!--top nav1 end--><!--utility nav start-->
-               <nav class="utility-nav">
-                  <span class="cms_phone">800.967.6630</span> <a href="mailto:sales@metalpartnersrebar.com" class="swap cms_email"><img src="/images/email-off.jpg" alt="Email" title="Email"></a>
-               </nav>
-               <!--utility nav end-->
-               <!--header social icon start-->
-               <div class="social-icon swap">
-                  <a href="https://www.linkedin.com/company/metal-partners-llc" rel="nofollow" target="_blank"><img src="/images/social-media/linkedin-off.jpg" alt="LinkedIn" title="LinkedIn"></a> <a href="https://www.youtube.com/channel/UCWI99FSCoh4zwQpcsymAMDA" rel="nofollow" target="_blank"><img src="/images/social-media/youtube-off.jpg" alt="YouTube" title="YouTube"></a> <a href="https://twitter.com/metalpartners" rel="nofollow" target="_blank"><img src="/images/social-media/twitter-off.jpg" alt="Twitter" title="Twitter"></a> <a href="https://plus.google.com/106165849516249473051/posts" target="_blank"><img src="/images/social-media/google-plus-off.jpg" alt="Google Plus" title="Google Plus" width="16" height="19"></a> <a href="https://metalpartnersrebar.wordpress.com/" rel="nofollow" target="_blank"><img src="/images/social-media/wordpress-off.jpg" alt="Wordpress" title="Wordpress"></a>
-               </div>
-               <!--header social icon end-->
-               <!--search trigger start--> <span class="search-trigger"><a href="javascript:void(0);"><img src="/images/search-btn.jpg" alt="Search" title="Search"></a></span>
-               <!--search trigger end-->
-               <!-- search start -->
-               <div id="search">
-                  <form method="get" action="/search.php">
-                     <input onblur="if( this.value=='' ) this.value='Search'" id="query" class="search-text" onfocus="if( this.value=='Search' ) this.value='';" value="Search" name="query">
-                     <input alt="Search" title="Search" class="search-btn" src="/images/search-btn.jpg" type="image">
-                     <input value="1" name="search" type="hidden">
-                  </form>
-               </div>
-               <!-- search end -->
-               <!--Click to call start--> <span class="click-to-call"><a href="https://service.ringcentral.com/mobile/ringme/callback.html?mbid=30EC193CA89AAE1DBCC62157F594CFDB941960005561,0,&amp;referer=&amp;s=" rel="nofollow" target="_blank"><span>Click to call us</span></a></span>
-               <!--Click to call end-->
-            </div>
-            <!--header right end-->
-         </div>
-         <!--header inner wrap end-->
-         <!--top nav2 start-->
-         <nav id="top-nav2">
-            <!--topnav2 inner wrap start-->
-            <div class="inner-wrap">
-               <!--menu trigger start--> <span class="menu-trigger"><a href="javascript:void(0);">Show Menu</a></span>
-               <!--menu trigger end-->
-               <!--top navigation2 links start-->
-               <ul class="top-nav2 cms_menu_2">
-                  <li class="has-child products"><a href="javascript:void(0);">Products<span></span></a>
-                     <ul class="cms_menu_3">
-                        <li><a href="/rebar-wire-mesh.html">Rebar &amp; Wire Mesh</a></li> <li><a href="/epoxy-coated-rebar.html">Epoxy Coated Rebar</a></li> <li><a href="/dowel-bars.html">Dowel Bars</a></li>
-                     </ul>
-                  </li><li class="has-child"><a href="javascript:void(0);">Services<span></span></a>
-                     <ul class="cms_menu_4">
-                        <li><a href="/custom-rebar-cutting-bending.html">Custom Cutting &amp; Bending of Rebar &amp; Dowel Bars</a></li> <li><a href="/distribution-sales-rebar.html">Rebar Distribution &amp; Sales</a></li>
-                     </ul>
-                  </li><li class="has-child"><a href="javascript:void(0);">Distribution by Industry Application<span></span></a>
-                     <ul class="cms_menu_5">
-                        <li><a href="/construction-supply-houses.html">Construction Supply Houses</a></li> <li><a href="/wind-towers.html">Wind Towers</a></li> <li><a href="/precast-concrete-fabricators.html">Precast Concrete Fabricators</a></li> <li><a href="/rebar-fabricators.html">Rebar Fabricators</a></li>
-                     </ul>
-                  </li><li><a href="/approved-dot-states.html">Approved DOT States</a></li><li><a href="/distribution.html">Distribution</a></li><li><a href="/customer-portal.html">Customer Portal</a></li>
-               </ul>
-               <!--top navigation2 links end-->
-            </div>
-            <!--topnav2 inner wrap end-->
-         </nav>
-         <!--top nav2 end-->
-    </header>
-     	<main class="inner-wrap"><!--content area start-->
